a5f8b1686ce27a716dd11638eaf278196501001a
Muhammad Habib Jawady <mohamedhabib.jaouadi@edu.isetcom.tn>
2023-04-22 19:04:47 +0100

    Design Patterns Welcome message

---
+++ src/main/java/com/learning/designpatterns/DesignPatterns.java
@@ -0,0 +1,8 @@
+package com.learning.designpatterns;
+
+public class DesignPatterns {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, design patterns in Java!");
+    }
+}
